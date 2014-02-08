f404e711b9ee42d976115ac05ea0f297e9d2439a
Bao Shenglin <Baoshenglin1994@gmail.com>
2014-02-08 20:34:24 +0800

    (1)Each client to provide a thread
    (2)Provide BindException

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -11,45 +11,80 @@ import java.io.*;
 
 public class ChatServer {
 
+	boolean started = false;
+	ServerSocket ss = null;
+
 	public static void main(String[] args) {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		boolean started = false;
-		ServerSocket ss = null;
-		Socket s = null;
+		new ChatServer().start();
+	}
+
+	public void start() {
 		try {
 			ss = new ServerSocket(8888);
+			started = true;
+		} catch (BindException e) {
+			System.out.println("The port is in use...");
+			System.out.println("Please close the related procedure and re run the server!");
+			System.exit(0);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 		try {
-			started = true;
 			while(started) {
-				boolean bConnected = false;
-				s = ss.accept();
+				Socket s = ss.accept();
+				Client c = new Client(s);
 System.out.println("a client connected!");
-				bConnected = true;
-				dos = new DataOutputStream(s.getOutputStream());
+				new Thread(c).start();
+				// dis.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	class Client implements Runnable {
+		private Socket s;
+		private DataInputStream dis = null;
+		private boolean bConnected = false;
+
+		public Client(Socket s) {
+			this.s = s;
+			try {
 				dis = new DataInputStream(s.getInputStream());
+				bConnected = true;
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+			try {
 				while(bConnected) {
 					String str = dis.readUTF();
 					System.out.println(str);
 				}
-				// dis.close();
-			}
-		} catch (IOException e) {
-			// e.printStackTrace();
-			System.out.println("A client closed!");
-		} finally {
-			try {
-				if(dis != null) {
-					dis.close();
-				}
-				if(s != null) {
-					s.close();
-				}
-			} catch(IOException e) {
+			} catch (EOFException e) {
+				System.out.println("A client closed!");
+			} catch (IOException e) {
 				e.printStackTrace();
+			} finally {
+				try {
+					if(dis != null) {
+						dis.close();
+					}
+					if(s != null) {
+						s.close();
+					}
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
